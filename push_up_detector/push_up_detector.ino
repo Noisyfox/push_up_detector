c26b866c23f01f157d7eb26607de553da53fab72
Lucas <Yulin@vlan-2621-10-16-88-16.student.wireless.sydney.edu.au>
2017-09-20 17:59:04 +1000

    Ultrasonic sensor (without test).

--- push_up_detector/push_up_detector.ino
+++ push_up_detector/push_up_detector.ino
@@ -1,5 +1,21 @@
+// defines pins numbers
+int trigPin = 9;
+int echoPin = 10;
+
+// defines variables
+long duration;
+int distance;
+
 void setup() {
   // put your setup code here, to run once:
+
+  // Ultrasonic sensor connection configuration
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+
+  Serial.begin(9600); // Starts the serial communication
+
 
 }
 
@@ -7,3 +23,21 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+int ultrasonic_sensor_data (int trigPin, int echoPin) {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds to ultrasonic sound
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance measured in centimeter
+  distance = duration * 0.034 / 2;
+}
+
