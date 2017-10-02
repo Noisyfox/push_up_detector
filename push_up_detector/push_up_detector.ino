042663243de1e25bc0fc003c672e93ce898d3edd
DESKTOP-EEE53H7\Yulin <yulin.nirvana.zhou@hotmail.com>
2017-10-02 19:09:08 +1100

    Buzzer and push-up detection.

--- push_up_detector/push_up_detector.ino
+++ push_up_detector/push_up_detector.ino
@@ -1,10 +1,19 @@
 // defines pins numbers
+int buzzPin = 8;
 int trigPin = 9;
 int echoPin = 10;
 
 // defines variables
+// the variables for ultrasonic sensor
 long duration;
 int distance;
+
+// count the number of push-up
+int count;
+
+// the variables for push up detection
+int flag1 = 1;
+int flag2 = 1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,15 +22,35 @@ void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
+  // buzz connection configuration
+  pinMode(buzzPin, OUTPUT);
+
 
   Serial.begin(9600); // Starts the serial communication
-
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  distance = ultrasonic_sensor_data (trigPin, echoPin);
 
+  Serial.println(distance);
+
+  if (distance < 20){
+    flag1 = 0;
+    flag2 = 0; 
+  }
+  else {
+    flag2 = 1;
+    if (flag2 != flag1){
+      flag1 = 1;
+      tone(buzzPin, 1000); // Send 1KHz sound signal
+      delay(50);        
+      noTone(buzzPin);     // Stop sound
+      delay(50);
+    }
+  }
+
+  delay(100);
 }
 
 int ultrasonic_sensor_data (int trigPin, int echoPin) {
@@ -39,5 +68,7 @@ int ultrasonic_sensor_data (int trigPin, int echoPin) {
   
   // Calculating the distance measured in centimeter
   distance = duration * 0.034 / 2;
+
+  return distance;
 }
 
